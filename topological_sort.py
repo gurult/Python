9bb3ec0e4167a067119ab20d9fe36e8417141c52
gurult <rusliz666@gmail.com>
2014-04-03 21:39:59 +0300

    added class Vertex
    to file topological_sort.py added supporting class  Vertex

---
+++ topological_sort.py
@@ -0,0 +1,19 @@
+﻿
+class Vertex:
+
+    def __init__(self, vertex):
+
+        self.vertex = vertex  # name of vertex
+        self.suns = []        # childs of vertex
+        self.father = -1      # father of vertex then sorted, can be only one
+        self.mark = -1        # not visited -1, temporary mark 0, visited 1
+
+    def print_info(self):
+
+        print()
+        # print("virsune " + str(self.vertex))
+        print("virsune %s" % self.vertex)
+        print("sunus " + str(self.suns))
+        print("tevas yra : " + str(self.father))
+        print("žyme: " + str(self.mark))
+        print()
