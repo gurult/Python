c408d2dedbb3f2a338f8682504eb0237365cc130
gurult <rusliz666@gmail.com>
2014-04-04 17:41:17 +0300

    Added new clas Topological_sort
    new class has methods read edges from file, and make make vertex_list
    which constructs  list of our graph edges

--- topological_sort.py
+++ topological_sort.py
@@ -1,4 +1,37 @@
-﻿
+﻿# author Ruslan Bulko
+
+
+import os
+
+
+class Topological_sort:
+
+    vertex_list = []
+
+    def __init__(self):
+        pass
+
+    def read_graph_edges_from_file(self, file_name):
+
+        if os.path.isfile(file_name):
+            with open(file_name) as f:
+                self.graph_edges = []
+                for line in f:  # read rest of lines
+                    self.graph_edges.append([int(x) for x in line.split(" ")])
+            f.close()
+
+    def make_vertex_list(self):
+        templist = set()
+        for element in self.graph_edges:
+            for wortex in element:
+                if wortex in templist:
+                    pass
+                else:
+                    self.vertex_list.append(Vertex(wortex))
+                    templist.add(wortex)
+        self.vertex_list.sort(key=lambda x: x.vertex)
+
+
 class Vertex:
 
     def __init__(self, vertex):
@@ -12,8 +45,8 @@ class Vertex:
 
         print()
         # print("virsune " + str(self.vertex))
-        print("virsune %s" % self.vertex)
-        print("sunus " + str(self.suns))
-        print("tevas yra : " + str(self.father))
-        print("žyme: " + str(self.mark))
+        print("Vertex : %s" % self.vertex)
+        print("Suns : " + str(self.suns))
+        print("Father is : " + str(self.father))
+        print("Mark : " + str(self.mark))
         print()
