08a4a2fe0ec88b56a2875d52ec95138d3832cf7d
gurult <rusliz666@gmail.com>
2014-04-07 00:33:42 +0300

    methods which was called by test.py know called by topological_sort.py
    functionality of test.py was inserted to topological_sort.py to simplify
    program for user and more code understability

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
 from topological_sort import Topological_sort
 
-grafas = Topological_sort()
-grafas.read_graph_edges_from_file("input.txt")
-grafas.make_vertex_list()
-print(grafas.graph_edges)
-# print("------------")
-grafas.find_suns_of_vertex()
-grafas.topological_sorting()
-grafas.print_info_of_votexes()
+graph = Topological_sort()
+graph.sort("input.txt")
